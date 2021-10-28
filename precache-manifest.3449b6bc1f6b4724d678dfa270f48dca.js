e1db2396328474fac7dec537d7f4e9c38494c558
sam_bland <sbland.co.uk@gmail.com>
2021-10-28 20:26:10 +0100

    Updates

---
+++ precache-manifest.3449b6bc1f6b4724d678dfa270f48dca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b8b012816dda4d21138959fd002d37",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3ffb18568ba1e6e3eb34",
+    "url": "./static/css/main.b1c5f9a6.chunk.css"
+  },
+  {
+    "revision": "93c59f90dd6059fe5b6e",
+    "url": "./static/js/2.3402d5d0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.3402d5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ffb18568ba1e6e3eb34",
+    "url": "./static/js/main.801d477c.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.801d477c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1662b9c794d5a3848bb",
+    "url": "./static/js/runtime-main.ef750e49.js"
+  }
+]);
